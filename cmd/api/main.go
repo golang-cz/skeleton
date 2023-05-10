f956dbe7eddc111b88a454b663406c2fe1fc1ba9
little_farmer <david.littlefarmer@gmail.com>
2023-05-10 10:13:11 +0200

    slog, environment, basic chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/golang-cz/skeleton/config"
 	"github.com/golang-cz/skeleton/internal/core"
+	"github.com/golang-cz/skeleton/pkg/graceful"
 	"github.com/golang-cz/skeleton/pkg/version"
 	"github.com/golang-cz/skeleton/services/api"
+	apiHttp "github.com/golang-cz/skeleton/services/api/http"
 )
 
 var (
@@ -45,26 +49,26 @@ func main() {
 
 	fmt.Println(app.Config.Sentry)
 
-	// defer app.Close()
+	defer app.Close()
 
 	// fmt.Info().Msgf("running application in %s environment version %s", api.App.Config.Environment.String(), version.VERSION)
 
-	// srv := &http.Server{
-	// 	Addr:              api.App.Config.Port,
-	// 	Handler:           apiHttp.Router(&rpc.ApiServer{}),
-	// 	IdleTimeout:       60 * time.Second, // idle connections
-	// 	ReadHeaderTimeout: 10 * time.Second, // request header
-	// 	ReadTimeout:       5 * time.Minute,  // request body
-	// 	WriteTimeout:      5 * time.Minute,  // response body
-	// 	MaxHeaderBytes:    1 << 20,          // 1 MB
-	// }
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           apiHttp.Router(),
+		IdleTimeout:       60 * time.Second, // idle connections
+		ReadHeaderTimeout: 10 * time.Second, // request header
+		ReadTimeout:       5 * time.Minute,  // request body
+		WriteTimeout:      5 * time.Minute,  // response body
+		MaxHeaderBytes:    1 << 20,          // 1 MB
+	}
 
-	// wait, _ := graceful.ShutdownHTTPServer(srv, time.Minute)
+	wait, _ := graceful.ShutdownHTTPServer(srv, time.Minute)
 
 	// zlog.Info().Msgf("API serving at %v", api.App.Config.Port)
-	// if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-	// 	log.Fatal(err)
-	// }
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 
-	// <-wait
+	<-wait
 }
---
+++ services/api/http/router.go
@@ -0,0 +1,53 @@
+package apiHttp
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/golang-cz/skeleton/config"
+	"github.com/golang-cz/skeleton/pkg/slogger"
+
+	"github.com/rs/cors"
+)
+
+func Router() chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.NoCache)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.RealIP)
+	r.Use(slogger.SloggerMiddleware)
+
+	if config.App.Environment.IsLocal() {
+		r.Use(middleware.Recoverer)
+	} else {
+		r.Use(middleware.Recoverer)
+	}
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: config.App.AllowedOrigins,
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+		AllowedHeaders: []string{
+			"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-FE-Route"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	r.Use(corsHandler.Handler)
+
+	r.Get("/robots.txt", robots)
+
+	r.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(""))
+	})
+
+	return r
+}
+
+func robots(w http.ResponseWriter, r *http.Request) {
+	// Disallow all robots. We don't want to be indexed by Google etc.
+	fmt.Fprintf(w, "User-agent: *\nDisallow: /\n")
+}
