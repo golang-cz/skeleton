dc8cef0b98c2aba9c749efb547941b5fcc741d37
little_farmer <david.littlefarmer@gmail.com>
2023-05-18 14:17:55 +0200

    json tags for UserStore

--- data/database/user.go
+++ data/database/user.go
@@ -9,11 +9,11 @@ import (
 
 type UserStore struct {
 	ID        uuid.UUID  `db:"id,omitempty,pk"       json:"id"`
-	Email     string     `db:"email"`
-	Firstname string     `db:"firstname"`
-	Lastname  string     `db:"lastname"`
-	CreatedAt *time.Time `db:"created_at"`
-	UpdatedAt *time.Time `db:"updated_at, omitempty"`
+	Email     string     `db:"email"                 json:"email"`
+	Firstname string     `db:"firstname"             json:"firstname"`
+	Lastname  string     `db:"lastname"              json:"lastname"`
+	CreatedAt *time.Time `db:"created_at"            json:"created_at"`
+	UpdatedAt *time.Time `db:"updated_at, omitempty" json:"updated_at"`
 }
 
 func (user *UserStore) Store(sess db.Session) db.Store {
